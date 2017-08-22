834e2ce021fc2f8b6e4df2e79e44b53861b8b3f7
Leonid Evdokimov <leon@darkk.net.ru>
2017-08-22 14:30:09 +0300

    moar ntpd heuristics

--- collector/ntp.go
+++ collector/ntp.go
@@ -25,18 +25,27 @@ import (
 	"github.com/prometheus/client_golang/prometheus"
 )
 
+const (
+	maxDispersion = 16 // aka MAXDISP
+	phi_us        = 15 // phi is 15e-6 (s)
+	maxPoll       = 17 // log2 max poll interval (~36 h)
+
+	maxDistance = (1500000 + (phi_us * (1 << maxPoll))) * time.Microsecond // 1.5s is MAXDIST from ntp.org, 1.0 is from RFC5905
+)
+
 var (
-	ntpServer          = flag.String("collector.ntp.server", "127.0.0.1", "NTP server to use for ntp collector.")
-	ntpServerIsMine    = flag.Bool("collector.ntp.server-is-mine", false, "Certify that collector.ntp.server address is managed by the same entity as this collector.")
-	ntpIpTTL           = flag.Int("collector.ntp.ip-ttl", 1, "IP TTL to use while sending NTP query.")
-	ntpProtocolVersion = flag.Int("collector.ntp.protocol-version", 4, "NTP protocol version.")
-	ntpOffsetTolerance = flag.Duration("collector.ntp.local-offset-tolerance", 0, "Offset between local clock and local ntpd time to tolerate.")
+	ntpServer          = flag.String("collector.ntp.server", "127.0.0.1", "NTP server to use for ntp collector")
+	ntpServerIsLocal   = flag.Bool("collector.ntp.server-is-local", false, "Certify that collector.ntp.server address is the same local host as this collector.")
+	ntpIpTTL           = flag.Int("collector.ntp.ip-ttl", 1, "IP TTL to use while sending NTP query")
+	ntpProtocolVersion = flag.Int("collector.ntp.protocol-version", 4, "NTP protocol version")
+	ntpMaxDistance     = flag.Duration("collector.ntp.max-distance", maxDistance, "Max accumulated distance to the root")
+	ntpOffsetTolerance = flag.Duration("collector.ntp.local-offset-tolerance", time.Millisecond, "Offset between local clock and local ntpd time to tolerate")
 
 	leapMidnight time.Time
 )
 
 type ntpCollector struct {
-	stratum, leap, sanity, rtt, offset, root_delay, root_dispersion typedDesc
+	stratum, leap, rtt, offset, reftime, root_delay, root_dispersion, sanity typedDesc
 }
 
 func init() {
@@ -49,7 +58,7 @@ func init() {
 // - the server is reachable with outgoin IP_TTL = 1
 func NewNtpCollector() (Collector, error) {
 	ipaddr := net.ParseIP(*ntpServer)
-	if !*ntpServerIsMine && (ipaddr == nil || !ipaddr.IsLoopback()) {
+	if !*ntpServerIsLocal && (ipaddr == nil || !ipaddr.IsLoopback()) {
 		return nil, fmt.Errorf("only IP address of local NTP server is valid for -collector.ntp.server")
 	}
 
@@ -82,14 +91,19 @@ func NewNtpCollector() (Collector, error) {
 			"ClockOffset.",
 			nil, nil,
 		), prometheus.GaugeValue},
+		reftime: typedDesc{prometheus.NewDesc(
+			prometheus.BuildFQName(Namespace, "ntp", "reftime"),
+			"ReferenceTime raw value.",
+			nil, nil,
+		), prometheus.GaugeValue},
 		root_delay: typedDesc{prometheus.NewDesc(
 			prometheus.BuildFQName(Namespace, "ntp", "root_delay"),
-			"RootDelay.",
+			"RootDelay raw value.",
 			nil, nil,
 		), prometheus.GaugeValue},
 		root_dispersion: typedDesc{prometheus.NewDesc(
 			prometheus.BuildFQName(Namespace, "ntp", "root_dispersion"),
-			"RootDispersion.",
+			"RootDispersion raw value.",
 			nil, nil,
 		), prometheus.GaugeValue},
 		sanity: typedDesc{prometheus.NewDesc(
@@ -101,9 +115,9 @@ func NewNtpCollector() (Collector, error) {
 }
 
 func (c *ntpCollector) Update(ch chan<- prometheus.Metric) error {
-	resp, err := ntp.QueryEx(*ntpServer, ntp.QOption{
+	resp, err := ntp.QueryWithOptions(*ntpServer, ntp.QueryOptions{
 		Version: *ntpProtocolVersion,
-		IpTTL:   *ntpIpTTL,
+		TTL:     *ntpIpTTL,
 		Timeout: time.Second, // default `ntpdate` timeout
 	})
 	if err != nil {
@@ -111,13 +125,10 @@ func (c *ntpCollector) Update(ch chan<- prometheus.Metric) error {
 		return fmt.Errorf("couldn't get SNTP reply: %s", err)
 	}
 
-	ch <- c.stratum.mustNewConstMetric(float64(resp.Stratum))
-	ch <- c.leap.mustNewConstMetric(float64(resp.Leap))
-
 	// Reference Timestamp: Time when the system clock was last set or
 	// corrected. Semantics of this value seems to vary across NTP server
 	// implementations: it may be both NTP-clock time and system wall-clock
-	// time of this event. :-( So (T2 - ReferenceTime) is not true
+	// time of this event. :-( So (T3 - ReferenceTime) is not true
 	// "freshness" as it may be actually NEGATIVE, so it's not exposed as
 	// metrics to avoid confusion.
 	freshness := resp.Time.Sub(resp.ReferenceTime)
@@ -126,35 +137,53 @@ func (c *ntpCollector) Update(ch chan<- prometheus.Metric) error {
 	// is required as ntp.org ntpd may report sane other fields while
 	// giving quite erratic clock. The check is declared in packet() at
 	// https://tools.ietf.org/html/rfc5905#appendix-A.5.1.1.
-	// Also, RFC5905 suggests more strict check in fit(), that suggest that
-	// root_delay should be lambda with some more summands (e.g. jitter)
-	// and be less than MAXDIST + PHI * LOG2D(s.poll). MAXPOLL is 17, so it
-	// is approximately at most (1s + 15e-6 * 2**17) = 2.96608 s, but
-	// MAXDIST and MAXPOLL are confugurable values in the reference
-	// implementation, so only MAXDISP check is done here.
-	lambda := resp.RootDelay / 2 + resp.RootDispersion
+	lambda := resp.RootDelay/2 + resp.RootDispersion
 
-	// (-1*RTT/2 <= ClockOffset <= RTT/2) is equal to (T1 <= T3 && T2 <= T4).
-	// That's not a true check that clock is NOT out-of-sync (T1 <= T2 <= T3 <= T4),
-	// but that's good enough as (T3 - T2) is usually small compared to (T4 - T1).
-	// It is used to detect the case when NTP client wall-clock differs
-	// from NTP-clock in the NTP server that is running on same machine (it
-	// should have quite similar clock). That's required for chrony as it
-	// starts relaying sane NTP clock before system wall-clock are actually
-	// adjusted.
+	// Also, RFC5905 suggests more strict check in fit(), that suggest that
+	// root_delay should be less than MAXDIST + PHI * LOG2D(s.poll).
+	// MAXPOLL is 17, so it is approximately at most (1s + 15e-6 * 2**17) =
+	// 2.96608 s, but MAXDIST and MAXPOLL are confugurable values in the
+	// reference implementation, so only MAXDISP check has hardcoded value.
+	// root_delay should also have following summands
+	// + Dispersion towards the peer
+	// + jitter of the link to the peer
+	// + PHI * (current_uptime - peer->uptime_of_last_update)
+	// but all these values are 0 if only single NTP packet was sent.
+	root_delay := (resp.RTT+resp.RootDelay)/2 + resp.RootDispersion
+
+	// (-1*RTT/2 <= ClockOffset <= RTT/2) is equal to (T1 <= T2 && T3 <= T4).
+	// That's not a true check that clock is in-sync (T1 <= T2 <= T3 <= T4),
+	// but that's good enough as clock usually goes forward, so I assume
+	// that T2 <= T3 and T1 <= T4.  It is used to detect the case when NTP
+	// client wall-clock differs from NTP-clock in the NTP server that is
+	// running on same machine (these two should be quite similar clock).
+	// That's required for chrony as it starts relaying sane NTP clock
+	// before system wall-clock is actually adjusted.
 	//
 	// ntpOffsetTolerance is added to avoid warning on following chrony
 	// state that is _practically_ sane: RTT = 0.000174662,
 	// ClockOffset = -0.000261665, Self-reported Offset = -0.000215618
-	offset_margin := resp.RTT / 2 + *ntpOffsetTolerance
+	offset_margin := resp.RTT/2 + *ntpOffsetTolerance
 	if resp.Leap == ntp.LeapAddSecond || resp.Leap == ntp.LeapDelSecond {
 		// state of leapMidnight is cached as leap flag is dropped right after midnight
-		leapMidnight = resp.Time.Truncate(24*time.Hour) + 24*time.Hour
+		leapMidnight = resp.Time.Truncate(24 * time.Hour).Add(24 * time.Hour)
 	}
-	if leapMidnight - 24*time.Hour <= resp.Time && resp.Time <= leapMidnight + 24*time.Hour {
+	if leapMidnight.Add(-24*time.Hour).Before(resp.Time) && resp.Time.Before(leapMidnight.Add(24*time.Hour)) {
+		// tolerate leap smearing
 		offset_margin += time.Second
 	}
 
+	ch <- c.stratum.mustNewConstMetric(float64(resp.Stratum))
+	ch <- c.leap.mustNewConstMetric(float64(resp.Leap))
+	var reftime float64
+	if resp.ReferenceTime.Unix() > 0 {
+		// Go Zero is   0001-01-01 00:00:00 UTC
+		// NTP Zero is  1900-01-01 00:00:00 UTC
+		// UNIX Zero is 1970-01-01 00:00:00 UTC
+		// so let's keep ALL ancient `reftime` values as zero
+		reftime = float64(resp.ReferenceTime.UnixNano()) / 1e9
+	}
+	ch <- c.reftime.mustNewConstMetric(reftime)
 	ch <- c.rtt.mustNewConstMetric(resp.RTT.Seconds())
 	ch <- c.offset.mustNewConstMetric(resp.ClockOffset.Seconds())
 	ch <- c.root_delay.mustNewConstMetric(resp.RootDelay.Seconds())
@@ -165,7 +194,8 @@ func (c *ntpCollector) Update(ch chan<- prometheus.Metric) error {
 		0 < resp.Stratum && resp.Stratum < ntp.MaxStratum &&
 		0 <= freshness && // from packet()
 		freshness <= 24*time.Hour && // 24h is heuristics from ntpdate
-		lambda <= 16*time.Second && // from packet()
+		lambda <= maxDispersion*time.Second && // from packet()
+		root_delay <= *ntpMaxDistance && // from fit()
 		0 <= resp.RTT && // ensuring that clock tick forward
 		-1*offset_margin <= resp.ClockOffset && resp.ClockOffset <= offset_margin {
 		sanity = 1.
